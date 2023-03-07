64b467dd24bdbce317add81913c0a0bb622be4ab
Puzyrnikow Andrey <daf1>
2023-03-07 22:46:37 +0400

    add eraser

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,22 +9,14 @@ public class Main {
                 new Person("Evgeni", "X Y Z G D L", 40),
                 new Person("Petr", "X Y", 155),
                 new Person("Georg", "X Y", 17),
-                new Person("Alex", "X-Y-Z", 22)
+                new Person("Alex", "X-Y-Z", 22),
+                new Person("Sasha", "A", 41),
+                new Person("Egor", "A", 15),
+                new Person("Julia", "A", 11)
         ));
-        Comparator<Person> comparator = (Person o1, Person o2) ->
-        {
-            int maxLenght = 3;
-            String[] words1 = o1.getSurname().split("(?U)\\W");
-            String[] words2 = o2.getSurname().split("(?U)\\W");
-            if (words1.length > maxLenght && words2.length > maxLenght)
-                return Integer.compare(o2.getAge(), o1.getAge());
-            else if (words1.length == words2.length) {
-                return Integer.compare(o2.getAge(), o1.getAge());
-            }
-            return Integer.compare(words1.length, words2.length);
-        };
+        Predicate<Person> predicate = x -> x.getAge() < 18;
+        people.removeIf(predicate);
+        System.out.println(people);
 
-        Collections.sort(people, comparator);
-        System.out.println(people.toString());
     }
 }
